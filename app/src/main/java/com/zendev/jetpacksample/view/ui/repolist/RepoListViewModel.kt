2402e37b6dae00685e11702a8ac78ad8fb8beb8d
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-06-04 18:00:35 +0700

    Update Class RepoListViewModel

--- app/src/main/java/com/zendev/jetpacksample/view/ui/repolist/RepoListViewModel.kt
+++ app/src/main/java/com/zendev/jetpacksample/view/ui/repolist/RepoListViewModel.kt
@@ -1,2 +1,23 @@
 package com.zendev.jetpacksample.view.ui.repolist
 
+import androidx.lifecycle.MutableLiveData
+import com.zendev.jetpacksample.model.Item
+import com.zendev.jetpacksample.model.RepoRepository
+import com.zendev.jetpacksample.view.base.BaseViewModel
+
+class RepoListViewModel : BaseViewModel() {
+    val repoListLive = MutableLiveData<List<Item>>()
+
+    fun fetchRepoList() {
+        dataLoading.value = true
+        RepoRepository.getInstance().getRepoList { isSuccess, response ->
+            dataLoading.value = false
+            if (isSuccess) {
+                repoListLive.value = response?.items
+                empty.value = false
+            } else {
+                empty.value = true
+            }
+        }
+    }
+}
