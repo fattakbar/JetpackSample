b1cc13a709b2ce032f627e9cca307f5378c3a685
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-06-04 14:53:05 +0700

    Update ApiService.kt

--- app/src/main/java/com/zendev/jetpacksample/model/api/ApiService.kt
+++ app/src/main/java/com/zendev/jetpacksample/model/api/ApiService.kt
@@ -1,2 +1,12 @@
 package com.zendev.jetpacksample.model.api
 
+import com.zendev.jetpacksample.model.GitResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("search/repositories")
+    fun getRepo(@Query("q") search: String = "trending", @Query("sort") sort: String = "stars"): Call<GitResponse>
+}
