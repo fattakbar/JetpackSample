6d7836d54bae7721d824149de04506eb05f4a68a
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-06-04 14:34:12 +0700

    Menambahkan Fragment repoDetail dan repoList

--- app/src/main/java/com/zendev/jetpacksample/view/ui/MainActivity.kt
+++ app/src/main/java/com/zendev/jetpacksample/view/ui/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.zendev.jetpacksample.view.ui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.zendev.jetpacksample.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        NavigationUI.setupActionBarWithNavController(this, findNavController(R.id.main_nav_fragment))
     }
+
+    override fun onSupportNavigateUp() = findNavController(R.id.main_nav_fragment).navigateUp()
 }
