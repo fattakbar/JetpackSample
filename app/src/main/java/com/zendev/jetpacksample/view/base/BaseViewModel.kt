588d2f19dbb01a968f00c906e0a7eef12fcf9266
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-06-04 17:59:17 +0700

    Update Class BaseViewModel

--- app/src/main/java/com/zendev/jetpacksample/view/base/BaseViewModel.kt
+++ app/src/main/java/com/zendev/jetpacksample/view/base/BaseViewModel.kt
@@ -1,2 +1,13 @@
 package com.zendev.jetpacksample.view.base
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel : ViewModel() {
+
+    val empty = MutableLiveData<Boolean>().apply { value = false }
+
+    val dataLoading = MutableLiveData<Boolean>().apply { value = false }
+
+    val toastMessage = MutableLiveData<String>()
+}
